e05c17c80b3e34ead2d1da3487071cc71ac26bed
buddhima300 <106884361+buddhima300@users.noreply.github.com>
2024-08-25 19:44:43 +0530

    Delete test_app.py

--- test_app.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from app import app  # Adjust this import if your file is named differently
-
-client = TestClient(app)
-
-def test_index():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Best university courses recommendation machine learning API"}
